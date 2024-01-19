c23eca54af90ad3e033763ddc6e03e36b0fd431a
zPavel39 <golubev33108ai@gmail.com>
2024-01-19 13:54:10 +0200

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import type { ManifestOptions, VitePWAOptions } from 'vite-plugin-pwa'
+import type { VitePWAOptions } from 'vite-plugin-pwa'
 import {VitePWA} from "vite-plugin-pwa";
 
 
@@ -32,30 +32,7 @@ const vitePWA: Partial<VitePWAOptions> = {
       },],
   },
 }
-const replaceOptions = { __DATE__: new Date().toISOString() }
-const claims = process.env.CLAIMS === 'true'
-const reload = process.env.RELOAD_SW === 'true'
-const selfDestroying = process.env.SW_DESTROY === 'true'
 
-if (process.env.SW === 'true') {
-  vitePWA.srcDir = 'src'
-  vitePWA.filename = claims ? 'claims-sw.ts' : 'prompt-sw.ts'
-  vitePWA.strategies = 'injectManifest'
-  ;(vitePWA.manifest as Partial<ManifestOptions>).name = 'PWA Inject Manifest'
-  ;(vitePWA.manifest as Partial<ManifestOptions>).short_name = 'PWA Inject'
-}
-
-if (claims)
-  vitePWA.registerType = 'autoUpdate'
-
-if (reload) {
-  // @ts-expect-error just ignore
-  replaceOptions.__RELOAD_SW__ = 'true'
-}
-
-if (selfDestroying)
-  vitePWA.selfDestroying = selfDestroying
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA(vitePWA)],
 })
