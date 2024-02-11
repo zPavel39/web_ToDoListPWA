8c9323571dd58ed4fc486afe031bca40769d5e1f
zPavel39 <golubev33108ai@gmail.com>
2024-02-11 06:20:55 +0200

    fix: config

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oh6dl79nvcg"
+    "revision": "0.6j5bs3ppepg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
